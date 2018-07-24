97e209412f081bb68dd47107aa30faa6f2e14b20
Tr4vi5 <trav.dunn@outlook.com>
2018-07-24 17:01:26 -0500

    Group Project Completed

--- client.js
+++ client.js
@@ -1,3 +1,10 @@
+$(document).ready(readyNow);
+
+function readyNow(){
+  console.log('JQuery');
+  $('#bonusButton').on('click', bonusCalc);
+}
+
 class Employee{
   constructor( name, employeeNumber, annualSalary, reviewRating ){
     this.name = name;
@@ -17,4 +24,66 @@ const employees = [ atticus, jem, scout, robert, mayella ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
-console.log( employees );
+function bonusCalc(){
+  for (let employee of employees){
+    if (employee.reviewRating === 5){
+      employee.bonusPercentage = 10;
+    } else if (employee.reviewRating === 4){
+      employee.bonusPercentage = 6;
+    } else if (employee.reviewRating === 3){
+      employee.bonusPercentage = 4;
+    } else if (employee.reviewRating <= 2){
+      employee.bonusPercentage = 0;
+    }
+  }
+  empDigits();
+  annualInc();
+  maxBonus();
+  actualBonus();
+  finalComp();
+  return employees;
+}
+
+
+function empDigits(){
+  for (let employee of employees){
+    if (employee.employeeNumber.length === 4){
+      employee.bonusPercentage += 5;
+    }
+  }
+  return employees;
+}
+
+function annualInc(){
+  for (let employee of employees){
+    if (employee.annualSalary > 65000 && employee.bonusPercentage > 0){
+      employee.bonusPercentage -= 1;
+    }
+  }
+  return employees;
+}
+
+function maxBonus(){
+  for (let employee of employees){
+    if (employee.bonusPercentage > 13){
+      employee.bonusPercentage = 13;
+    }
+  }
+  return employees;
+}
+
+function actualBonus(){
+  for (let employee of employees){
+    employee.totalBonus = (Number(employee.annualSalary) * (employee.bonusPercentage * 0.01)).toFixed(2);
+  }
+  return employees;
+}
+
+function finalComp(){
+  for (let employee of employees){
+    employee.totalCompensation = (Number(employee.totalBonus) + Number(employee.annualSalary)).toFixed(2);
+  }
+  return employees;
+}
+
+console.log(bonusCalc());
